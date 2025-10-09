81b017fb3f1f948f11867b4054ffabee052f2f2b
Moussa <salisoumoussa1@gmail.com>
2025-10-09 16:46:36 +0200

    Update Scuola.Desktop

---
+++ Scuola.Desktop/LoginPage.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Scuola.Desktop
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            txtNomeUtente.Text = lblRisultato.Text;
+        }
+    }
+}
